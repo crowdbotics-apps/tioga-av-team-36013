7080107ec5e3b7a0b87b7458d5d34d5b0cf9f59e
Crowdbotics <team@crowdbotics.com>
2022-07-01 23:19:42 +0000

    Update models

--- backend/tioga_av_team_36013/wsgi.py
+++ backend/tioga_av_team_36013/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tioga_av_team_36013.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tioga_av_team_36013.settings")
 
 application = get_wsgi_application()
