310b625a1ae613d3feb3a77264113eaf2026fd10
Crowdbotics <team@crowdbotics.com>
2022-07-01 22:36:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tioga_av_team_36013",
+  url: "https://tioga_av_team_36013.botics.co",
+  api: "https://tioga_av_team_36013.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
